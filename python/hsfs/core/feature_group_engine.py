7cce99f0b5120f5043cf85e576988ff3e15a4deb
Till Döhmen <till.doehmen@web.de>
2022-06-28 18:59:05 +0200

    move missing snippet to .insert

--- python/hsfs/core/feature_group_engine.py
+++ python/hsfs/core/feature_group_engine.py
@@ -99,6 +99,12 @@ class FeatureGroupEngine(feature_group_base_engine.FeatureGroupBaseEngine):
         write_options,
         validation_options,
     ):
+        if len(feature_group.features) == 0:
+            # User didn't provide a schema. extract it from the dataframe
+            feature_group._features = engine.get_instance().parse_schema_feature_group(
+                feature_dataframe
+            )
+
         # deequ validation only on spark
         validation = feature_group._data_validation_engine.ingest_validate(
             feature_group, feature_dataframe
