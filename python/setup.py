be50b63408a12a57208819a15e1a6b20ac2ee026
tdoehmen <till.doehmen@web.de>
2023-05-09 19:35:55 +0200

    added python version

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ def read(fname):
 setup(
     name="hsfs",
     version=__version__,
-    python_requires=">=3.7,<3.11",
+    python_requires=">=3.8,<3.11",
     install_requires=[
         "pyhumps==1.6.1",
         "requests",
